c5e351b0f8afce1283c64f4416e757e0b5bf5b3c
roronya <roronya628@gmail.com>
2018-04-09 06:38:13 +0900

    server

--- javascripting/regex-strings.js
+++ javascripting/regex-strings.js
@@ -4,5 +4,6 @@ correct zipcode 111-2222
 incorrect zipcode 12345678
 incorrect zipcode 1111-222
 `;
-const results = zipcodes.match(/\d{7}|\d{3}-\d{4}/g).map(zipcode => zipcode.trim());
+
+const results = zipcodes.match(/[^\d-]\d{7}[^\d-]|[^\d-]\d{3}-\d{4}[^\d-]/g).map(zipcode => zipcode.trim());
 console.log(results);
---
+++ todo-manager/server/todo-router.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const todos = [];
+
+router.post('/', (req, res, next) => {
+});
+module.exports = router;
